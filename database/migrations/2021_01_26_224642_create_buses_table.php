c6cf47f21adac5730e09d4d94d3b3f2c7374cbd6
alejosoccer <aelr238@gmail.com>
2021-01-31 18:20:53 -0500

    Laravel controladores

---
+++ database/migrations/2021_01_26_224642_create_buses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cooperativa_id')->constrained('cooperativas');
+            $table->foreignId('asiento_id')->constrained('asientos');
+            $table->foreignId('destino_id')->constrained('destinos');
+            $table->string('nombre', 300);
+            $table->string('capacidad', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buses');
+    }
+}
---
+++ database/migrations/2021_01_26_225341_create_reservas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservas', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha_reserva');
+            $table->time('hora_reserva');
+            $table->string('estado', 6)->default(true);
+            $table->string('tipo_bus', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservas');
+    }
+}
